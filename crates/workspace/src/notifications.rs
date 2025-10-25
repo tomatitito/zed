2cf6a9f26d9d9f640f394cb7ce2dca1f2f6088c6
Jens Kouros <jens.kouros@posteo.de>
2025-10-25 21:33:00 +0200

    Auto-hide language server notifications that do not require user interaction

--- crates/workspace/src/notifications.rs
+++ crates/workspace/src/notifications.rs
@@ -99,6 +99,29 @@ impl Workspace {
                 }
             })
             .detach();
+
+            if let Ok(prompt) =
+                AnyEntity::from(notification.clone()).downcast::<LanguageServerPrompt>()
+            {
+                if prompt
+                    .read(cx)
+                    .request
+                    .as_ref()
+                    .map_or(false, |r| r.actions.is_empty())
+                {
+                    cx.spawn({
+                        let id = id.clone();
+                        async move |this, cx| {
+                            cx.background_executor().timer(Duration::from_secs(5)).await;
+                            this.update(cx, |workspace, cx| {
+                                workspace.dismiss_notification(&id, cx);
+                            })
+                            .ok()
+                        }
+                    })
+                    .detach();
+                }
+            }
             notification.into()
         });
     }
