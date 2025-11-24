186bf42ed54ba304211ed1dd38c5caeae20cd508
Jens Kouros <jens.kouros@posteo.de>
2025-11-24 21:57:01 +0100

    Use AtomicUsize instead of random u64

--- crates/workspace/src/workspace.rs
+++ crates/workspace/src/workspace.rs
@@ -1331,7 +1331,7 @@ impl Workspace {
                     struct LanguageServerPrompt;
 
                     this.show_notification(
-                        NotificationId::composite::<LanguageServerPrompt>(request.id as usize),
+                        NotificationId::composite::<LanguageServerPrompt>(request.id),
                         cx,
                         |cx| {
                             cx.new(|cx| {
